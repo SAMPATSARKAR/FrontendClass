3b9d052e3b5780d0d43821feb43856ec42d9ab49
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-09 11:47:31 +0530

    Completed Array

---
+++ JS/Array/main.js
@@ -0,0 +1,5 @@
+let a = prompt("Enter a string:");
+console.log("LowerCase:",a.toLowerCase());
+console.log("replace space with - :",a.replaceAll(" ","-"));
+console.log("Extract first 10 lines: ",a.slice(0,10));
+
