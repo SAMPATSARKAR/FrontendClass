5338056f143a24666c7c519013292531869aa9e5
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-09 11:56:18 +0530

    Completed Array

--- JS/Array/main.js
+++ JS/Array/main.js
@@ -1,5 +1,6 @@
-let a = prompt("Enter a string:");
-console.log("LowerCase:",a.toLowerCase());
-console.log("replace space with - :",a.replaceAll(" ","-"));
-console.log("Extract first 10 lines: ",a.slice(0,10));
-
+let arr1=[10,20,30,40,50];
+console.log(arr1);
+console.log(arr1[4]);
+let arr2 =[10,'hello',20,'how',30,'are'];
+console.log(arr2[3]);
+console.log(arr2.length);
