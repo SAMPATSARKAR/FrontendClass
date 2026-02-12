7d1892c21a38fd4c5539c702d9af5e2b655d7518
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-12 15:30:49 +0530

    Completed methods in Js

---
+++ JS/function/methods/main.js
@@ -0,0 +1,30 @@
+// let fruits=["Apple","Banana","Orange","grapes","mango"];
+// console.log(fruits);
+// console.log(fruits.push("guava"));
+// console.log(fruits);
+// console.log(fruits.pop(1));
+// console.log(fruits);
+
+let fruits1=["waytermelon","papaya","kiwi","pineapple","strawberry"];
+// console.log(fruits1);
+//shift
+// console.log(fruits1.shift());//deletes first element
+// console.log(fruits1);
+//unshift
+// console.log(fruits1.unshift("dragonfruit"));//adds element at first position
+// console.log(fruits1);
+//slice
+// let fruits2=fruits1.slice(2,4);
+// console.log(fruits2);
+
+//splice
+// console.log(fruits1);
+// fruits1.splice(1,2,"coconuts");
+// console.log(fruits1);
+
+let comp = ["Amazon","flipkart","Uber","Microsoft"];
+console.log(comp.pop());
+console.log(comp.shift());
+console.log(comp.splice(1,1,"Ola"));
+console.log(comp);
+
