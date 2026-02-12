89baa3351764b5fe8fcdf8f099632ca8e881d30b
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-12 14:27:54 +0530

    function

---
+++ JS/function/main.js
@@ -0,0 +1,37 @@
+// function greeting(){
+//     console.log("Hello, World!");
+// }
+// greeting();
+
+// function add(a, b){
+//     return a + b;
+// }
+// console.log(add(3,4));
+// function even(a){
+//     for(let i=1;i<a;i++){
+//         if(i%2==0){
+//             console.log(i);
+            
+//         }
+
+//     }
+// }
+// even(10);
+
+// const greet=(x)=>{
+//     console.log(`Hello, ${x}`);
+// };
+// greet("sampat");
+
+// const add = (a,b)=> a+b;
+// console.log(add(4,5));
+
+const even=(a)=>{
+    for(let i=1;i<a;i++){
+        if(i%2==0){
+            console.log(i);
+            
+        }
+    }
+}
+even(10);
